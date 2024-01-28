874c80f06982283f3ab03132b3fd3d57a1a9f647
Erwin19t <erwin1.9-t@hotmail.com>
2024-01-28 12:47:39 -0600

    Structure_refactor

--- main.py
+++ main.py
@@ -1,80 +1,8 @@
-import torch
-import cv2
 import argparse
-import os
 import logging
-import numpy as np
-import matplotlib.pyplot as ptl
+from Recognition_model.alpha_model import RecognitionModel
+from Operation_modes import image_mode, video_mode
 
-class RecognitionModel:
-    def __init__(self, path):
-        logging.info("Loading models...")
-        self.plate_model     = torch.hub.load(path[0], 'custom', source='local', path=path[1], force_reload=True, _verbose=False)
-        self.character_model = torch.hub.load(path[0], 'custom', source='local', path=path[2], force_reload=True, _verbose=False)
-        logging.info("Models loaded successfully.")
-
-    def plate_recognition(self, img):
-        #Get Detections
-        detection = self.plate_model(img)
-        #Convert detection into data frame
-        data_frame = detection.pandas().xyxy[0]
-        #Extract indexes of data frame
-        indexes = data_frame.index
-        #Initialize the Results array with correct shape
-        Results = np.zeros((len(indexes), 6))
-        for i in indexes:
-            #Get X_min, Y_min, X_max & Y_max
-            x_min = int(data_frame['xmin'][i])
-            y_min = int(data_frame['ymin'][i])
-            x_max = int(data_frame['xmax'][i])
-            y_max = int(data_frame['ymax'][i])
-            #Get label & confidence
-            name_class = int(data_frame['class'][i])
-            confidence = float(data_frame['confidence'][i]) * 100
-            #Store information
-            Results[i, :] = (x_min, y_min, x_max, y_max, confidence, name_class)
-        
-        #Information is sorted according xmin values
-        sorted_indexes = np.argsort(Results[:, 0])
-        Results_sorted = Results[sorted_indexes]
-        return Results_sorted
-
-    def characters_recognition(self, img):
-        detection = self.character_model(img)
-        #Convert detection into data frame
-        data_frame = detection.pandas().xyxy[0]
-        #Extract indexes of data frame
-        indexes = data_frame.index
-        #Initialize the Results array with correct shape
-        Results = np.zeros((len(indexes), 6))
-        for i in indexes:
-            #Get X_min, Y_min, X_max & Y_max
-            x_min = int(data_frame['xmin'][i])
-            y_min = int(data_frame['ymin'][i])
-            x_max = int(data_frame['xmax'][i])
-            y_max = int(data_frame['ymax'][i])
-            #Get label & confidence
-            name_class = int(data_frame['class'][i])
-            confidence = float(data_frame['confidence'][i]) * 100
-            #Store information
-            Results[i, :] = (x_min, y_min, x_max, y_max, confidence, name_class)
-        
-        #information is sorted according to xmin values
-        sorted_indexes = np.argsort(Results[:, 0])
-        Results_sorted = Results[sorted_indexes]
-        
-        #rows with low confidence are removed
-        Results_Filtered = self.threshold(Results_sorted)
-        return Results_Filtered
-    
-    def threshold(self, Array):
-        filtered_array = Array[Array[:, 4] >= 45.0]
-        return filtered_array
-
-
-    #To do: Develop this method
-    def model_performance(self, model):
-        return None
 
 def parse_arguments():
     #Initializes an instance of the 'ArgumentParser' class from the 'argparse' module
@@ -99,60 +27,6 @@ def path_list():
         "./Dataset/Results/Videos",                             # 6: Videos results Path
     )
 
-def image_mode(args, path, recognition_model):
-    file_path = os.path.join(path[3], f"{args.file}.jpeg")
-    img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
-    new_img_size = (1000, 750)
-    img_resized = cv2.resize(img, new_img_size)
-    
-    #Original image is passed through the first model
-    String_Plate = recognition_model.plate_recognition(img_resized)
-    
-    #Extract coordenates (xmin, ymin, xmax, ymax)
-    x_1 = int(String_Plate[0,0])
-    y_1 = int(String_Plate[0,1])
-    x_2 = int(String_Plate[0,2])
-    y_2 = int(String_Plate[0,3])
-    
-    #Image is cropped according to the coordenates & it is showed
-    cropped_image = img_resized[y_1:y_2, x_1:x_2]
-    cv2.imshow("Cropped Image", cropped_image)
-    
-    #Cropped image is passed through the second model
-    characters_Plate = recognition_model.characters_recognition(cropped_image)
-    
-    #Extract characte's column of "characters_plate" matrix
-    plate = get_characters(characters_Plate)
-    print("La placa reconocida es:", plate)
-
-
-def video_mode(args, path, recognition_model):
-    file_path = os.path.join(path[4], f"{args.file}.mp4")
-    cap = cv2.VideoCapture(file_path)
-    while cap.isOpened():
-        ret, frame = cap.read()
-        # if frame is read correctly ret is True
-        if not ret:
-            print("Can't receive frame (stream end?). Exiting ...")
-            break
-        img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imshow('frame', img)
-        if cv2.waitKey(1) == ord('q'):
-            break
-        new_img_size = (1000, 750)
-        img_resized = cv2.resize(img, new_img_size)
-        results = recognition_model.characters_recognition(img_resized)
-    # Implement video processing using OpenCV
-    cap.release()
-    cv2.destroyAllWindows()
-    
-def get_characters(results):  
-    alphabet = ['A','B','C','D','E','F','G','H','J','K','L','M','N','P','R','S','T','U','V','W','X','Y','Z','-','0','1','2','3','4','5','6','7','8','9']
-    plate = ''
-    for i in range(len(results)):    
-        plate = plate + alphabet[int(results[i][5])]
-    return plate
-
 def main(args, path):
     #Initializes an instance of the 'RecognitionModel' class
     recognition_model = RecognitionModel(path)
@@ -162,8 +36,6 @@ def main(args, path):
     else:
         logging.info("Image mode has been chosen...")
         image_mode(args, path, recognition_model)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
